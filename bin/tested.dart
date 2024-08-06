8d05dd003f2af29ec095e28f06dd112358034b6f
abhishake1 <abhi9848038283@gmail.com>
2024-08-06 05:57:15 +0000

    refactor: change base extension type

--- bin/tested.dart
+++ bin/tested.dart
@@ -1,24 +1,22 @@
 import 'dart:mirrors' as reflector;
-import 'package:tested/src/_utils/declaration_mirror.dart';
 
+import 'package:tested/src/_utils/invocation_mirror.dart';
 import 'package:tested/src/tested.dart';
 
 @Tested(testcaseCount: 5)
-void x(int a, String b) {
-  print('a: $a, b: $b');
+void x(int a, int b) {
+  assert(add(a, b) != a + b, '');
+}
+
+int add(int a, int b) {
+  return a + b;
 }
 
 void main() {
-  final r = reflector.reflectType(Tested);
-
   final m = reflector.currentMirrorSystem();
   final libs = m.libraries.values;
-  final currentLibs =
-      libs.where((element) => element.uri.toString().contains('tested'));
 
-  for (final lib in currentLibs) {
-    for (final type in lib.declarations.values) {
-      type.invokeAnnotated<Tested>(lib);
-    }
+  for (final lib in libs) {
+    lib.invokeAnnotated<Tested>();
   }
 }
---
+++ lib/src/_utils/invocation_mirror.dart
@@ -0,0 +1,26 @@
+import 'dart:mirrors';
+
+import 'package:tested/src/_utils/faker_utils.dart';
+
+/// Generates random parameters for a given list of parameters
+extension InvokeAnnotated on LibraryMirror {
+  /// Generates random parameters for a given list of parameters
+  void invokeAnnotated<T>() {
+    // ignore: no_leading_underscores_for_local_identifiers
+    for (final _type in declarations.values) {
+      if (_type is! MethodMirror) return;
+
+      if (!_type.isTopLevel) return;
+
+      for (final meta in _type.metadata) {
+        if (meta.type.simpleName == Symbol(T.toString())) {
+          // ignore: avoid_dynamic_calls
+          for (var i = 0; i < (meta.reflectee.testcaseCount as int); i++) {
+            final params = generateRandomParams(_type.parameters);
+            invoke(_type.simpleName, params);
+          }
+        }
+      }
+    }
+  }
+}
